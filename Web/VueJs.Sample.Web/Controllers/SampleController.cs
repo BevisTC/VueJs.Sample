2bdd441b7044549294e242fa7cc71775ccc5f8d9
BevisTC <bevis@binodata.com>
2019-09-20 21:33:39 +0800

    add vue.js

---
+++ Web/VueJs.Sample.Web/Controllers/SampleController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VueJs.Sample.Web.Controllers
+{
+    public class SampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult DoAction([FromBody] RegisteViewModel model)
+        {
+            
+            return Json("data");
+        }
+
+
+
+
+    }
+
+    public class RegisteViewModel
+    {
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public bool Agree { get; set; }
+
+        public int Gender { get; set; }
+    }
+}
---
+++ Web/VueJs.Sample.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VueJs.Sample.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
